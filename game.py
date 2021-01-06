3687dbb70200d596eb07ace9098c86258ce81a23
Vicki Erjavec <verjavec@gmail.com>
2021-01-06 03:45:44 +0000

    Add check to see if player number equals random number

--- game.py
+++ game.py
@@ -10,16 +10,18 @@ print (f'Hello {name}!')
 #choose a random number
 
 numbertoguess = random.randrange(1,100)
+print(numbertoguess)
 
 #repeat this part until random number equals the guess 
 #ask player to input a number from 1-100
 
 str_guess = input("Please choose a number between 1 and 100: ")
-print(str_guess)
 guess = int(str_guess)
-print(guess)
 
 #check if input number is equal to random number
+
+if guess == numbertoguess:
+    print(f'Congratulations {name}! You guessed the number!!!')
 
 #increase number of guesses
 #if random number is not equal to input number, is it higher?
