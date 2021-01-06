146a928dca0e14c1625fcef5198f69457146d27f
Vicki Erjavec <verjavec@gmail.com>
2021-01-06 03:41:39 +0000

    make the player number and integer

--- game.py
+++ game.py
@@ -3,15 +3,24 @@
 import random
 # Put your code here
 # Greet the player
+
 name = input("Please enter your name: ")
 print (f'Hello {name}!')
 
 #choose a random number
+
 numbertoguess = random.randrange(1,100)
+
 #repeat this part until random number equals the guess 
 #ask player to input a number from 1-100
-guess = input("Please choose a number between 1 and 100: ")
+
+str_guess = input("Please choose a number between 1 and 100: ")
+print(str_guess)
+guess = int(str_guess)
+print(guess)
+
 #check if input number is equal to random number
+
 #increase number of guesses
 #if random number is not equal to input number, is it higher?
 #if random number is not equal to or higher, it must be lower
